70248c8664460c452db6712036460976a329b0be
Samuel Colvin <s@muelcolvin.com>
2025-09-01 10:15:52 -0700

    Prep 0.0.25 release

--- uprev.sh
+++ uprev.sh
@@ -3,7 +3,9 @@
 set -e
 
 if [ "$#" -ne 1 ]; then
-  echo "Current version: `uvx --from=toml-cli toml get --toml-path=packages/python/pyproject.toml project.version`"
+  echo "Current version:     `uvx --from=toml-cli toml get --toml-path=packages/python/pyproject.toml project.version`"
+  echo "PyPI latest version: `curl -s https://pypi.org/pypi/genai-prices/json | jq -r '.info.version'`"
+  echo "NPM latest version:  `curl -s https://registry.npmjs.org/@pydantic/genai-prices/latest | jq -r '.version'`"
   echo "Usage: $0 <new-version>"
   exit 1
 fi
