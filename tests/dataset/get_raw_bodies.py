04038b0c6b9ee8b19867c5e432745371f5fa9570
Alex Hall <alex.mojaki@gmail.com>
2025-11-04 15:15:21 +0200

    usages

---
+++ tests/dataset/get_raw_bodies.py
@@ -0,0 +1,19 @@
+import json
+from typing import Any
+
+import yaml
+from utils import raw_bodies_path, this_dir
+
+bodies: list[Any] = []
+for f in (this_dir / '../../../pydantic-ai/tests').rglob('*.yaml'):
+    text = f.read_text()
+    if len(text) > 3_000_000:
+        continue
+    parsed = yaml.load(text, yaml.Loader)
+    interactions = parsed.get('interactions', [])
+    for interaction in interactions:
+        parsed_body = interaction.get('response', {}).get('parsed_body', {})
+        if parsed_body:
+            bodies.append(parsed_body)
+
+raw_bodies_path.write_text(json.dumps(bodies))
